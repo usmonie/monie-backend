4849ce48d38d2d6ad7678c8dabcf8bb8c2bf1b8e
Usman Akhmedov <u.akhmedov@fhl.world>
2022-12-11 00:29:07 +0300

    updated for scalable

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::net::SocketAddr;
+use tokio::net::TcpListener;
+
 use tokio::sync::mpsc;
 use tonic::transport::Server;
 
@@ -9,32 +12,51 @@ mod domain;
 mod data;
 mod authentication;
 
-#[tokio::main]
-async fn main() -> Result<(), tonic::transport::Error> {
-    let addrs = ["0.0.0.0:8080"];
-    let (tx, mut rx) = mpsc::unbounded_channel();
-
-    for addr in addrs {
-        let tx = tx.clone();
-
-        let authentication_service = AuthenticationService {};
-        let authentication_server = AuthenticationServer::new(authentication_service);
-
-        let addr = addr.parse().unwrap();
-        let serve = Server::builder()
-            .concurrency_limit_per_connection(256)
-            .add_service(authentication_server)
-            .serve(addr);
-
-        tokio::spawn(async move {
-            if let Err(e) = serve.await {
-                eprintln!("Error = {:?}", e);
-            }
-
-            tx.send(()).unwrap();
+fn main(){
+    let mut handlers = Vec::new();
+    for i in 0..num_cpus::get() {
+        let h = std::thread::spawn(move || {
+            tokio::runtime::Builder::new_current_thread()
+                .enable_all()
+                .build()
+                .unwrap()
+                .block_on(serve());
         });
-    };
+        handlers.push(h);
+    }
 
-    rx.recv().await;
-    Ok(())
+    for h in handlers {
+        h.join().unwrap();
+    }
+}
+
+async fn serve() {
+    let addr: SocketAddr = "0.0.0.0:8080".parse().unwrap();
+    let sock = socket2::Socket::new(
+        match addr {
+            SocketAddr::V4(_) => socket2::Domain::IPV4,
+            SocketAddr::V6(_) => socket2::Domain::IPV6,
+        },
+        socket2::Type::STREAM,
+        None,
+    ).unwrap();
+
+    let authentication_service = AuthenticationService {};
+    let authentication_server = AuthenticationServer::new(authentication_service);
+
+    sock.set_reuse_address(true).unwrap();
+    sock.set_reuse_port(true).unwrap();
+    sock.set_nonblocking(true).unwrap();
+    sock.bind(&addr.into()).unwrap();
+    sock.listen(8192).unwrap();
+
+    let incoming =
+        tokio_stream::wrappers::TcpListenerStream::new(TcpListener::from_std(sock.into()).unwrap());
+
+    Server::builder()
+        .concurrency_limit_per_connection(256)
+        .add_service(authentication_server)
+        .serve_with_incoming(incoming)
+        .await
+        .unwrap();
 }
